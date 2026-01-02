9006e64becca3b22963a8bc2cefc135fd80796fc
ParkCH753 <ckdgusqkr0803@gmail.com>
2026-01-02 10:46:17 +0900

    HttpServletResponse 객체

---
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,34 @@
+package hello.servlet.basic.response;
+
+import hello.servlet.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Content-Type 지정
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setAge(20);
+        helloData.setUsername("Park");
+
+        // {"username" : "kim" ,"age" : 20}
+        String result = objectMapper.writeValueAsString(helloData);
+
+        resp.getWriter().write(result);
+    }
+}
